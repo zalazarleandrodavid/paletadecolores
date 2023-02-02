44701289b70e6dac3c32e4e8b09214fb0142b266
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2023-02-02 17:39:44 -0300

    codigo de paleta

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 const paletaContainer= document.getElementById('paletaContainer');
 const colorRange = document.getElementById('colorRange');
 const colorValues = ['1','2','3','4','6','7','8','9','A','B','C','D','E','F'];
-const PALLETE_SIZE = 5;
+const PALETA_SIZE = 5;
 
 const createPaleta =() => {
-    for (let i = 0; i < PALLETE_SIZE; i++) {
+    for (let i = 0; i < PALETA_SIZE; i++) {
         const paletaElement = document.createElement('div');
         paletaElement.classList.add('paletaItem');
         paletaContainer.appendChild(paletaElement);
     }
+    updatePaleta();
 };
 
+
+
+const colorear =(paletaElement) => {
+    let color='#'
+    for(let i = 0; i < 6; i++){
+        const randomElement= colorValues[Math.floor(Math.random() *colorValues.length)];
+        color += randomElement;
+    };
+    paletaElement.style.backgroundColor = color;
+    paletaElement.innerHTML = `<span class='colorHex'>${color}</span>`;
+}
+
+const updatePaleta = () => {
+    for (let i = 0; i < paletaContainer.children.length; i++) {
+        colorear(paletaContainer.children[i])
+    }
+    
+};  
+
+
 createPaleta();
+
+
